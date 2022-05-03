a5d92ae107abc9078058b316a9fbf8b319915961
Eduardo Schettino <schettino72@gmail.com>
2022-05-04 02:56:42 +0800

    support async tests. add test_runner.py.

--- src/rut/runner.py
+++ src/rut/runner.py
@@ -1,9 +1,9 @@
 import inspect
 import logging
+import asyncio
 
 from rich.console import Console
 from rich.syntax import Syntax
-from rich import inspect as rich_inspect
 
 from .checker import TestFailure
 from .collect import Collector
@@ -21,49 +21,67 @@ class Runner:
         fixtures = {}
         for mod_name, case_name, case in collector.iter_cases():
             log.info('run %s::%s' % (mod_name, case_name))
+            is_coro = inspect.iscoroutinefunction(case.func)
             try:
+
+                # kwargs contain fixtures values for dependency injection
                 kwargs = {}
                 if case_fixtures := getattr(case.func, 'use_fix', None):
                     for name, fix_func in case_fixtures.items():
                         fixtures[name] = fix_func()
                         kwargs[name] = next(fixtures[name])
+
                 if case.cls:
                     obj = case.cls()
-                    case.func(obj, **kwargs)
+                    if is_coro:
+                        asyncio.run(case.func(obj, **kwargs))
+                    else:
+                        case.func(obj, **kwargs)
                 else:
-                    case.func(**kwargs)
+                    if is_coro:
+                        asyncio.run(case.func(**kwargs))
+                    else:
+                        case.func(**kwargs)
+
+                # fixture cleanup
                 if fixtures:
                     for name in fixtures.keys():
                         next(fixtures[name], None)  # should I check StopIteration was raised?
 
             except TestFailure as failure:
                 result = 'FAIL'
-                console.print()
-                console.rule(f"[bold red]{case_name}", style="red")
-                tb = failure.__traceback__
-                while tb:
-                    frame = tb.tb_frame
-                    tb = tb.tb_next
-                    if frame.f_globals['__package__'] == 'rut':
-                        continue
-                    # rich_inspect(tb, all=True)
-                    code = frame.f_code
-                    console.print(f"{code.co_filename}:{frame.f_lineno}: {failure.__class__.__name__}")
-                    console.print(Syntax(
-                        inspect.getsource(code),
-                        'python',
-                        start_line=code.co_firstlineno,
-                        line_numbers=True,
-                        highlight_lines=set([frame.f_lineno]),
-                    ))
-                for arg in failure.args:
-                    console.print(arg)
-                console.rule(style="bright_black", characters="━")
+                self._print_failure(case_name, failure)
             except Exception:
                 result = 'ERROR'
                 console.print_exception(show_locals=True)
             else:
-                result = 'PASS'
+                result = 'SUCCESS'
                 console.print(f"{mod_name}::{case_name}: [green]OK[/green]")
 
             case.result = result
+
+    def _print_failure(self, case_name, failure: TestFailure):
+        console.print()
+        console.rule(f"[bold red]{case_name}", style="red")
+        tb = failure.__traceback__
+        while tb:
+            frame = tb.tb_frame
+            tb = tb.tb_next
+            if frame.f_globals['__package__'] == 'rut':
+                continue
+            # rich_inspect(tb, all=True)
+            code = frame.f_code
+            console.print(f"{code.co_filename}:{frame.f_lineno}: {failure.__class__.__name__}")
+            try:
+                console.print(Syntax(
+                    inspect.getsource(code),
+                    'python',
+                    start_line=code.co_firstlineno,
+                    line_numbers=True,
+                    highlight_lines=set([frame.f_lineno]),
+                ))
+            except OSError:  # can not getsource of code
+                pass
+        for arg in failure.args:
+            console.print(arg)
+        console.rule(style="bright_black", characters="━")
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,3 +1,99 @@
+import types
 
-def test_runner():
+from rut import check
+
+from rut.collect import Collector
+from rut.runner import Runner
+
+
+
+def add_test_cases(collector, src, name='this_test'):
+    """create module from src text, and add its TestCase's to collector"""
+    # FIXME: include source
+    # https://stackoverflow.com/questions/62160529/how-to-get-source-inside-exec-python
+    module = types.ModuleType(name)
+    exec(src, module.__dict__)
+    collector.mods.append(name)
+    collector.cases[name] = collector._collect_module_tests(module)
+    return module
+
+
+class TestRunnerExecute:
+    def test_run_func(self):
+        collector = Collector()
+        src = """
+def test_one():
     assert True
+"""
+        add_test_cases(collector, src)
+        check(collector.cases['this_test']['test_one'].result) == None
+        Runner().execute(collector)
+        check(collector.cases['this_test']['test_one'].result) == 'SUCCESS'
+
+    def test_run_method(self):
+        collector = Collector()
+        src = """
+class TestMyClass:
+    def test_moo(self):
+        assert True
+"""
+        add_test_cases(collector, src)
+        check(collector.cases['this_test']['TestMyClass.test_moo'].result) == None
+        Runner().execute(collector)
+        check(collector.cases['this_test']['TestMyClass.test_moo'].result) == 'SUCCESS'
+
+
+    def test_run_async(self):
+        collector = Collector()
+        src = """
+track = []
+async def some_val(val):
+    return val
+
+async def test_foo():
+    track.append(await some_val('foo'))
+    assert True
+
+class TestMyClass:
+    async def test_moo(self):
+        track.append(await some_val('moo'))
+        assert True
+"""
+        module = add_test_cases(collector, src)
+        check(collector.cases['this_test']['test_foo'].result) == None
+        check(collector.cases['this_test']['TestMyClass.test_moo'].result) == None
+        check(module.track) == []
+        Runner().execute(collector)
+        check(module.track) == ['foo', 'moo']
+        check(collector.cases['this_test']['test_foo'].result) == 'SUCCESS'
+        check(collector.cases['this_test']['TestMyClass.test_moo'].result) == 'SUCCESS'
+
+
+
+class TestRunnerResult:
+    def test_error(self):
+        collector = Collector()
+        src = """
+def inner():
+    5/0
+def test_one():
+    inner()
+    assert True
+"""
+        add_test_cases(collector, src)
+        check(collector.cases['this_test']['test_one'].result) == None
+        Runner().execute(collector)
+        check(collector.cases['this_test']['test_one'].result) == 'ERROR'
+
+    def test_fail(self):
+        collector = Collector()
+        src = """
+from rut import check
+def test_one():
+    calculated = 6
+    check(6) == 3 + 2
+"""
+        add_test_cases(collector, src)
+        check(collector.cases['this_test']['test_one'].result) == None
+        Runner().execute(collector)
+        check(collector.cases['this_test']['test_one'].result) == 'FAIL'
