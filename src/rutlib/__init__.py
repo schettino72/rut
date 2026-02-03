e5a6a877833086aca0eba5fbe37fc6560fac022d
Eduardo Schettino <schettino72@gmail.com>
2026-02-03 15:22:40 +0800

    release 0.3.0

--- src/rutlib/__init__.py
+++ src/rutlib/__init__.py
@@ -1,9 +1,6 @@
 __all__ = ["RutCLI", "RutRunner", "WarningCollector", "RutError", "InvalidAsyncTestError"]
-__version__ = "0.3.0.dev0"
+__version__ = "0.3.0"
 __license__ = "MIT"
-__author__ = "Eduardo Naufel Schettino"
-__author_email__ = "schettino72@gmail.com"
-__url__ = "https://github.com/schettino72/rut"
 
 from .cli import RutCLI
 from .runner import RutRunner, WarningCollector, RutError, InvalidAsyncTestError
