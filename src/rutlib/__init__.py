f6dd825912b38507263d3b1f5cc7ac36b1cd7fff
Eduardo Schettino <schettino72@gmail.com>
2026-02-12 13:30:47 +0800

    start dev 0.4.

--- src/rutlib/__init__.py
+++ src/rutlib/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["RutCLI", "RutRunner", "WarningCollector", "RutError", "InvalidAsyncTestError"]
-__version__ = "0.3.0"
+__version__ = "0.4.0.dev0"
 __license__ = "MIT"
 
 from .cli import RutCLI
